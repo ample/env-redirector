3ab3885348415613a31e6cf1b4224a4085975f05
Taylor C. MacDonald <tcmacdonald@gmail.com>
2020-02-11 11:02:07 -0500

    Refactor to ensure redirect even if an exception is thrown

--- functions/env-redirector/env-redirector.js
+++ functions/env-redirector/env-redirector.js
@@ -1,25 +1,17 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context, callback) => {
-  try {
+  let dest = "https://pushpay.com/g/crossroads";
 
+  try {
     const referer = event.headers.referer
     const conditions = ['localhost', '0.0.0.0', '127.0.0.1', 'int', 'demo', 'development', 'release']
-    let dest = "https://pushpay.com/g/crossroads";
-
     if(referer && conditions.some(el => referer.includes(el))) {
       dest = "https://sandbox.pushpay.io/g/crossroadscincinnati"
     }
+  } catch (err) {}
 
-    const response = {
-      statusCode: 307,
-      headers: {
-        Location: dest,
-      },
-      body: JSON.stringify(event)
-    };
-    return callback(null, response);
-
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
+  return callback(null, {
+    statusCode: 307,
+    headers: { Location: dest }
+  });
 };
