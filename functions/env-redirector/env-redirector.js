735631954689717dd150783cb5e79f16a8b0518f
Taylor C. MacDonald <tcmacdonald@gmail.com>
2020-02-10 20:30:09 -0500

    Add redirect

--- functions/env-redirector/env-redirector.js
+++ functions/env-redirector/env-redirector.js
@@ -1,15 +1,24 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   try {
-    console.log("request: " + JSON.stringify(event))
-    const subject = event.queryStringParameters.name || "World";
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+
+    const referer = event.headers.referer
+    const conditions = ['localhost', '0.0.0.0', '127.0.0.1', 'int', 'demo', 'development', 'release']
+    let dest = "https://pushpay.com/g/crossroads";
+
+    if(referer && conditions.some(el => referer.includes(el))) {
+      dest = "https://sandbox.pushpay.io/g/crossroadscincinnati"
+    }
+
+    const response = {
+      statusCode: 301,
+      headers: {
+        Location: dest,
+      },
+      body: JSON.stringify(event)
     };
+    return callback(null, response);
+
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
